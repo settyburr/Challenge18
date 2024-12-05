3723acf78353555863724a34b5583cdff8f5e250
Seth Burrow <sethburrow01@gmail.com>
2024-12-05 09:16:00 -0600

    __direname

--- server/src/server.ts
+++ server/src/server.ts
@@ -23,10 +23,10 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join('../client/dist')));
 
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join('../client/dist/index.html'));
     });
   }
   
